d4d3e2077c1be32b1e83ec06386011e75ace3eb6
Joseph VTX <1395609@senati.pe>
2022-11-01 15:31:54 -0500

    nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -13,8 +13,8 @@ export const Nav = () => {
         <nav className='nav flex items-center justify-between h-[3.625rem] px-[1.125rem]'>
 
             <img src="../icons/menu.svg" alt="menu" onClick={handleMenu} />
-            <div className='flex'>
-                <img src="../icons/python.svg" alt="" />
+            <div className='flex gap-[0.5rem]'>
+                <img src="../icons/python.svg" alt="Python" />
                 <h1 className='title font-daysone'>Pythonistas</h1>
             </div>
 
